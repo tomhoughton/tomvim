d7ac4075a761dcd909f52ed3a428db0ad0935999
Tom Houghton <tom@smithmyers.com>
2024-10-14 10:03:42 +0100

    Added telescope

--- nvim/lua/config/setup.lua
+++ nvim/lua/config/setup.lua
@@ -1,5 +1,10 @@
 -- Theme
 vim.cmd("colorscheme rose-pine")
--- Change
+
+-- KEYBINDINGS -- 
 vim.keymap.set("n", "<leader>fw", "<cmd>w<cr>", { silent = true, desc = "Write" })
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)  
+
+-- telescope:
+local telebuiltin = require('telescope.builtin')
+vim.keymap.set("n", "<leader>ff", telebuiltin.find_files, { desc = "Telescope find files" }) 
