bcb65e7cfe5039b46dbddf39f50ad843944202c2
Thomas Houghton <tomhoughton888@gmail.com>
2024-10-14 16:28:53 +0100

    Added mappings and file browser

--- nvim/lua/config/setup.lua
+++ nvim/lua/config/setup.lua
@@ -12,4 +12,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- telescope:
 local telebuiltin = require('telescope.builtin')
-vim.keymap.set("n", "<leader>ff", telebuiltin.find_files, { desc = "Telescope find files" }) 
+vim.keymap.set("n", "<leader>ff", telebuiltin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", telebuiltin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", telebuiltin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", telebuiltin.help_tags, { desc = "Telescope help tags" })
